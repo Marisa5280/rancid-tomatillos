c22670702fb1992d5fd6d768959f3f12e1191c61
Marisa Wyatt <risawyatt22@gmail.com>
2023-10-13 16:22:23 -0600

    Run full data through a cleaning function

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,18 @@
-import logo from '../../logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import movieData from '../../mockMovies';
 
 function App() {
-
+  const homepageData = movieData.movies.map((movie) => {
+    return {
+     id: movie.id,
+     title: movie.title,
+     image: movie.backdrop_path
+    }
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
