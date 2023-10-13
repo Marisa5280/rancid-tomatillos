c509e83957e3264c3af2c453e75c2d7f96fc7874
Marisa Wyatt <risawyatt22@gmail.com>
2023-10-13 16:34:59 -0600

    Render a title in the header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,12 @@ import { useState, useEffect } from 'react';
 import movieData from '../../mockMovies';
 
 function App() {
+  const [fulldata, setFullData] = useState({});
+  const [filteredData, setFilteredData] = useState([]);
+
+useEffect(() => {
+  setFullData(movieData);
+
   const homepageData = movieData.movies.map((movie) => {
     return {
      id: movie.id,
@@ -10,9 +16,16 @@ function App() {
      image: movie.backdrop_path
     }
   });
+
+  setFilteredData(homepageData);
+}, []);
+
   return (
     <div className="App">
-
+      <header>
+        <h1>Rancid Tomatillos</h1>
+        </header>
+      
     </div>
   );
 }
