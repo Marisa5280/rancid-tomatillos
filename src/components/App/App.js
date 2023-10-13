4f1191b2902baf7aa106386e04ba3a3d302a208c
Marisa Wyatt <risawyatt22@gmail.com>
2023-10-13 17:10:57 -0600

    Create movie cards on page load

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,33 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import movieData from '../../mockMovies';
-import Header from '../Header/Header';
-import MainContentWrapper from '../MainContentWrapper/MainContentWrapper';
+import "./App.css";
+import { useState, useEffect } from "react";
+import movieData from "../../mockMovies";
+import Header from "../Header/Header";
+import MainContentWrapper from "../MainContentWrapper/MainContentWrapper";
+import MovieCards from "../MovieCards/MovieCards";
 
 function App() {
   const [fulldata, setFullData] = useState({});
   const [filteredData, setFilteredData] = useState([]);
 
-useEffect(() => {
-  setFullData(movieData.movies);
+  useEffect(() => {
+    setFullData(movieData.movies);
 
-  const homepageData = movieData.movies.map((movie) => {
-    return {
-     id: movie.id,
-     title: movie.title,
-     image: movie.backdrop_path
-    }
-  });
+    const homepageData = movieData.movies.map((movie) => {
+      return {
+        id: movie.id,
+        title: movie.title,
+        image: movie.backdrop_path,
+      };
+    });
 
-  setFilteredData(homepageData);
-}, []);
+    setFilteredData(homepageData);
+  }, []);
 
   return (
     <div className="App">
-     <Header />
-     <MainContentWrapper/>
-      
+      <Header />
+      <MainContentWrapper />
+      <MovieCards filteredData={filteredData} />
     </div>
   );
 }
---
+++ src/components/MovieCards/MovieCards.js
@@ -0,0 +1,12 @@
+function MovieCards({ filteredData }) {
+  return filteredData.map((movie) => {
+    return (
+      <div id={`${movie.id}`}>
+        <img src={movie.image} height="25%" width="25%" />
+        <h2>{`${movie.title}`}</h2>
+      </div>
+    );
+  });
+}
+
+export default MovieCards;
