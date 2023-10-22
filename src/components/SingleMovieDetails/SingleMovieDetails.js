3dc0651f1f60e3370170d03f32a0b9a62ffdfec0
Marisa Wyatt <risawyatt22@gmail.com>
2023-10-22 12:40:53 -0600

    feat: Breadcrumb persists on page refresh, add shape to object prop type

--- src/components/SingleMovieDetails/SingleMovieDetails.js
+++ src/components/SingleMovieDetails/SingleMovieDetails.js
@@ -88,7 +88,19 @@ export default SingleMovieDetails;
 
 SingleMovieDetails.propTypes = {
   selectedMovie: PropTypes.number.isRequired,
-  singleMovieDetail: PropTypes.object,
+  singleMovieDetail: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    backdrop_path: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+    overview: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+    budget: PropTypes.number.isRequired,
+    revenue: PropTypes.number.isRequired,
+    runtime: PropTypes.number.isRequired,
+    tagline: PropTypes.string.isRequired,
+    average_rating: PropTypes.number.isRequired}),
   setSelectedMovie: PropTypes.func.isRequired,
   setError: PropTypes.func.isRequired,
   getIndividualMovie: PropTypes.func.isRequired,
